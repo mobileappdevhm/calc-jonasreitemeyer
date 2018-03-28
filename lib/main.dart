6c1128b7d9f6463a04e726a120efa16d08b485a7
Jonas Reitemeyer <jonas.reitemeyer@munichmotorsport.de>
2018-03-28 21:31:26 +0200

    Adds scrollable Output and travis file

--- lib/main.dart
+++ lib/main.dart
@@ -23,7 +23,7 @@ class Calculator extends StatefulWidget {
 
 class CalcState extends State<Calculator> {
 
-  String inputString = "2 x 3 = 4";
+  String outputString = "2 x 3 = 4";
   double l_value;
   double r_value;
   double result;
@@ -33,83 +33,87 @@ class CalcState extends State<Calculator> {
 
     setState(() {
 
-      if (inputString.contains("=")) {
-        inputString = "";
+      if (outputString.contains("=")) {
+        outputString = "";
       }
 
       switch(buttonValue) {
         case "C":
-          inputString = "";
+          outputString = "";
           break;
         case "<":
-          if (inputString.length > 1){
-            String _inputString = inputString.substring(0, inputString.length - 2);
-            inputString = _inputString;
+          if (outputString.length > 1){
+            String _inputString = outputString.substring(0, outputString.length - 2);
+            outputString = _inputString;
           }
           break;
         case ".":
-          inputString = inputString.substring(0, inputString.length - 1);
-          inputString += ".";
+          outputString = outputString.substring(0, outputString.length - 1);
+          outputString += ".";
           break;
         case "=":
 
-          if(inputString[inputString.length -1 ] == " "){
-            inputString = inputString.substring(0, inputString.length - 1);
+          if(outputString[outputString.length -1 ] == " "){
+            outputString = outputString.substring(0, outputString.length - 1);
           }
 
 
-          List<String> components = inputString.split(" ");
+          List<String> components = outputString.split(" ");
 
           if(components.length != 3) {
-            inputString = "Nicht unterstützt";
+            outputString = "Nicht unterstützt";
           }
 
           print(components);
 
           // try {
-            l_value = double.parse(components[0]);
-            r_value = double.parse(components[2]);
-            print("Left Value: " + l_value.toString());
-            print("Right Value: " + r_value.toString());
+          l_value = double.parse(components[0]);
+          r_value = double.parse(components[2]);
+          print("Left Value: " + l_value.toString());
+          print("Right Value: " + r_value.toString());
 
-            mat_op = components[1];
-            print("Operator: " + mat_op);
+          mat_op = components[1];
+          print("Operator: " + mat_op);
 
-            switch(mat_op) {
-              case "%":
-                result = l_value % r_value;
-                break;
-              case "/":
+          switch(mat_op) {
+            case "%":
+              result = l_value % r_value;
+              break;
+            case "/":
+              if (r_value != 0.0){
                 result = l_value / r_value;
-                break;
-              case "x":
-                result = l_value * r_value;
-                break;
-              case "-":
-                result = l_value - r_value;
-                break;
-              case "+":
-                result = l_value + r_value;
-                break;
-            }
+              } else {
+                result = 13.37;
+              }
+              break;
+            case "x":
+              result = l_value * r_value;
+              break;
+            case "-":
+              result = l_value - r_value;
+              break;
+            case "+":
+              result = l_value + r_value;
+              break;
+          }
 
-            print("Result: " + result.toString());
+          print("Result: " + result.toString());
 
-            if(result == result.round()) {
-              inputString += " = " + result.round().toString();
-            } else {
-              inputString += " = " + result.toString();
-            }
+          if(result == result.round()) {
+            outputString += " = " + result.round().toString();
+          } else {
+            outputString += " = " + result.toString();
+          }
 
           // } catch (e, stackTrace) {
           //   print(e);
           //   print(stackTrace);
-          //   inputString = "Ungültiges Format";
+          //   outputString = "Ungültiges Format";
           // }
 
           break;
         default:
-          inputString += buttonValue + " ";
+          outputString += buttonValue + " ";
           break;
 
       }
@@ -130,38 +134,44 @@ class CalcState extends State<Calculator> {
                 color: Colors.white.withOpacity(0.85),
                 child: new Row(
                   children: <Widget>[
-                    new Text(
-                      inputString,
-                      overflow: TextOverflow.clip,
-                      maxLines: 2,
-                      softWrap: true,
-                      style: new TextStyle(
-                        color: Colors.black87,
-                        fontWeight: FontWeight.w700,
-                        fontSize: 40.0,
+                    new Expanded (
+                      child : new SingleChildScrollView (
+                        scrollDirection: Axis.horizontal,
+                        child : new Text(
+                          outputString,
+                          overflow: TextOverflow.clip,
+                          maxLines: 2,
+                          softWrap: true,
+                          style: new TextStyle(
+                            color: Colors.black87,
+                            fontWeight: FontWeight.w700,
+                            fontSize: 40.0,
+                            ),
+                          ),
+                        ),
+                      ),
+                  ],
+
+                  ),
+                  ),
+                  ),
+                  new Expanded(
+                      flex: 4,
+                      child: new Container(
+                        child: new Column(
+                          children: <Widget>[
+                            makeBtns('C<%/'),
+                            makeBtns('789x'),
+                            makeBtns('456-'),
+                            makeBtns('123+'),
+                            makeBtns('0.='),
+                          ],
+                          ),
                         ),
                       )
-                  ],
-                  ),
-                ),
-              ),
-            new Expanded(
-                flex: 4,
-                child: new Container(
-                  child: new Column(
-                    children: <Widget>[
-                      makeBtns('C<%/'),
-                      makeBtns('789x'),
-                      makeBtns('456-'),
-                      makeBtns('123+'),
-                      makeBtns('0.='),
                     ],
                     ),
-                  ),
-                )
-              ],
-              ),
-              );
+                    );
   }
 
   Widget makeBtns(String row) {
