b2118c54b668cf7e4190af9e0da875f7e649519e
Jonas Reitemeyer <jonas.reitemeyer@munichmotorsport.de>
2018-03-29 17:45:54 +0200

    minor

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -14,16 +14,10 @@ void main() {
     // Build our app and trigger a frame.
     await tester.pumpWidget(new MyApp());
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
+    // Verify that our display gets initialized correctly
+    expect(find.text('2 x 3 = 4'), findsOneWidget);
     expect(find.text('1'), findsNothing);
 
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
 
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
   });
 }
